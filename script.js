550f915789dd61ec7a5bf3df8d9191ae21a90d62
xtophe xanon <xocon10@gmail.com>
2020-06-04 22:48:04 -0500

    get length of password from user

--- script.js
+++ script.js
@@ -5,6 +5,26 @@ var allUpChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
 var allNums = "0123456789".split('');
 var allSpcChars = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".split('');
 
+function generatePassword() {
+    // PROMPT FOR PASSWORD LENGTH & VALIDATE RESPONSE
+    var numChars = prompt("How many characters would you like in your password (Enter a value between 8 and 128)");
+    // allow user to cancel
+    if (numChars === null) {
+      return;
+    }
+  
+    // coerce NaN if input is a text string that cannot be coerced to a number
+    numChars = parseInt(numChars);
+  
+    // validate input
+    while (isNaN(numChars) || numChars < 8 || numChars > 128) {
+      if (numChars === null) {
+        return;
+      }
+      numChars = prompt("Invalid entry, please enter a NUMBER between 8 and 128");
+    }
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
